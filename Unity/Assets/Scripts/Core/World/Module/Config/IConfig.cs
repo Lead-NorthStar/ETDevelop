039b1d978e465963a4e35353c85036fa46055c9e
Lead-NorthStar <Lead-NorthStar@outlook.com>
2024-02-21 19:01:07 +0800

    遗漏提交

---
+++ Unity/Assets/Scripts/Core/World/Module/Config/IConfig.cs
@@ -0,0 +1,12 @@
+namespace ET
+{
+    /// <summary>
+    /// 每个Config的基类
+    /// 每个Config单例的基类
+    /// 用于生命周期管理
+    /// </summary>
+    public interface IConfig
+    {
+        void ResolveRef();
+    }
+}
